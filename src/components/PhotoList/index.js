cb71efdacc4e28fa4eb29a81afa07675f0fa75ba
Tony Rivera <rtonycloud@gmail.com>
2021-10-26 00:20:53 -0400

    completed partial react build

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 const PhotoList = ({ category }) => {
 
   const [photos] = useState([
@@ -107,10 +108,10 @@ const PhotoList = ({ category }) => {
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-          src={require(`../../assets/small/${category}/${i}.jpg`)}
-          alt={image.name}
-          className="img-thumbnail mx-1"
-          key={image.name}
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            alt={image.name}
+            className="img-thumbnail mx-1"
+            key={image.name}
           />
         ))}
       </div>
